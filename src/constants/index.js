b123da8e7012feb1d1c6bad6ec9bc736f5dc19ff
Madan Narra <narra.madan@outlook.com>
2020-05-01 20:28:47 +1000

    updated catalogue label

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,9 @@
 export const categoryOptions = [
-    { value: 'Books', label: 'categories.books' },
-    { value: 'Music', label: 'categories.music' },
-    { value: 'Movies', label: 'categories.movies' },
-    { value: 'Games', label: 'categories.games' },
-    { value: 'Electronics', label: 'categories.electronics' },
-    { value: 'Computers', label: 'categories.computers' },
-    { value: 'Office', label: 'categories.office' }
+    { value: 'Books', label: 'categories.Books' },
+    { value: 'Music', label: 'categories.Music' },
+    { value: 'Movies', label: 'categories.Movies' },
+    { value: 'Games', label: 'categories.Games' },
+    { value: 'Electronics', label: 'categories.Electronics' },
+    { value: 'Computers', label: 'categories.Computers' },
+    { value: 'Office', label: 'categories.Office' }
 ];
